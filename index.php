0f785615d388ab821ca0c5b2d08fb50fb2593dc0
Warat Kaweepornpoj <winwanwon123@gmail.com>
2016-01-09 09:23:53 +0700

    no message

--- index.php
+++ index.php
@@ -121,154 +121,234 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6 text-center">
-          <div class="page-header text-left"><h2>Upcoming Live Videos</h2></div>
+          <div class="page-header text-left"><h2>Now On Air</h2></div>
           <img src="img/video_frame.png" class="img-responsive" alt="">
+          <div class="text-left vid-description">
+            <h3>Lorem ipsum</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae quis officiis cupiditate quaerat magnam rerum asperiores saepe nostrum in maxime natus aut provident, rem soluta tenetur voluptatum quasi illo consequuntur?</p>
+          </div>
         </div>
         <div class="col-md-6 text-center">
           <div class="page-header text-left"><h2>Previous</h2></div>
           <div class="row">
             <div class="col-md-6">
               <img src="img/video_frame.png" class="img-responsive" alt="">
+              <div class="text-left vid-description vid-description-small">
+                <h4>Lorem ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae quis officiis cupiditate.</p>
+              </div>
             </div>
             <div class="col-md-6">
               <img src="img/video_frame.png" class="img-responsive" alt="">
+              <div class="text-left vid-description vid-description-small">
+                <h4>Lorem ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae quis officiis cupiditate.</p>
+              </div>
             </div>
           </div>
           <br>
           <div class="row">
             <div class="col-md-6">
-              <img src="img/video_frame.png" class="img-responsive" alt=""></div>
-              <div class="col-md-6">
-                <img src="img/video_frame.png" class="img-responsive" alt=""></div>
+              <img src="img/video_frame.png" class="img-responsive" alt="">
+              <div class="text-left vid-description vid-description-small">
+                <h4>Lorem ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae quis officiis cupiditate.</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <img src="img/video_frame.png" class="img-responsive" alt="">
+              <div class="text-left vid-description vid-description-small">
+                <h4>Lorem ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae quis officiis cupiditate.</p>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section id="video_list_prehigh">
-        <div class="container">
-          <div class="page-header">
-            <h2>มัธยมต้น</h2>
+    <section id="video_list_prehigh">
+      <div class="container">
+        <div class="page-header">
+          <h2>มัธยมต้น</h2>
+        </div>
+        <div class="row">
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
+            </div>
           </div>
-          <div class="row">
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
+          </div>
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
-            </div>
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
+          </div>
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section id="video_list_high">
-        <div class="container">
-          <div class="page-header">
-            <h2>มัธยมปลาย</h2>
-          </div>
-          <div class="row">
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
-            </div>
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
-            </div>
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
-            </div>
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
+    <section id="video_list_high">
+      <div class="container">
+        <div class="page-header">
+          <h2>มัธยมปลาย</h2>
+        </div>
+        <div class="row">
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
+          </div>
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
-            <div class="col-md-3 text-center">
-              <div class="service-box subject-box">
-                <h3>Sturdy Templates</h3>
-                <p class="text-muted">Our templates are updated regularly so they don't break.</p>
-              </div>
+          </div>
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
           </div>
         </div>
-      </section>
-
-
-      <section id="contact" class="bg-dark">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 text-center">
-              <h2 class="section-heading">Let's Get In Touch!</h2>
-              <hr class="primary">
-              <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+        <div class="row">
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
-            <div class="col-lg-4 col-lg-offset-2 text-center">
-              <i class="fa fa-phone fa-3x wow bounceIn"></i>
-              <p>123-456-6789</p>
+          </div>
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
-            <div class="col-lg-4 text-center">
-              <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
-              <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
+          </div>
+          <div class="col-md-3 text-center">
+            <div class="service-box subject-box">
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <!-- jQuery -->
-      <script src="js/jquery.js"></script>
+    <section id="articles">
+      <div class="container">
+        <div class="page-header">
+          <h2>Articles</h2>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="service-box subject-box">
+              <h3>Test Article</h3>
+              <p class="text-muted">Test Content.</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="service-box subject-box">
+              <h3>Test Article</h3>
+              <p class="text-muted">Test Content.</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="service-box subject-box">
+              <h3>Test Article</h3>
+              <p class="text-muted">Test Content.</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="service-box subject-box">
+              <h3>Test Article</h3>
+              <p class="text-muted">Test Content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="service-box subject-box">
+              <h3>Test Article</h3>
+              <p class="text-muted">Test Content.</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="service-box subject-box">
+              <h3>Test Article</h3>
+              <p class="text-muted">Test Content.</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="service-box subject-box">
+              <h3>Test Article</h3>
+              <p class="text-muted">Test Content.</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="service-box subject-box">
+              <h3>Test Article</h3>
+              <p class="text-muted">Test Content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
-      <!-- Bootstrap Core JavaScript -->
-      <script src="js/bootstrap.min.js"></script>
+    <section id="contact" class="bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-lg-offset-2 text-center">
+            <h2 class="section-heading">Let's Get In Touch!</h2>
+            <hr class="primary">
+            <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+          </div>
+          <div class="col-lg-4 col-lg-offset-2 text-center">
+            <i class="fa fa-phone fa-3x wow bounceIn"></i>
+            <p>123-456-6789</p>
+          </div>
+          <div class="col-lg-4 text-center">
+            <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+            <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
 
-      <!-- Plugin JavaScript -->
-      <script src="js/jquery.easing.min.js"></script>
-      <script src="js/jquery.fittext.js"></script>
-      <script src="js/wow.min.js"></script>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
 
-      <!-- Custom Theme JavaScript -->
-      <script src="js/creative.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
 
-    </body>
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
 
-    </html>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+
+  </body>
+
+  </html>
